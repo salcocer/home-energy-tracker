bf968e0aef6d21b2fc8545f452428be212ec2253
Alejandro Alcocer <saavalencia97@gmail.com>
2026-09-07 03:35:27 +0200

    add new device-service

---
+++ device-service/src/test/java/com/project/device_service/DeviceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.device_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
